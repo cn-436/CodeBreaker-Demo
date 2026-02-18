328ab566aec64be7ee5beb8acbc7370bcda898be
Jack <jack@dev.it>
2026-02-18 15:27:25 +0700

    Part 5

---
+++ CodeBreaker/Views/ElapsedTime.swift
@@ -0,0 +1,18 @@
+//
+//  ElapsedTime.swift
+//  CodeBreaker
+//
+//  Created by Weerachai Anotaipaiboon on 18/2/2569 BE.
+//
+
+import SwiftUI
+
+struct ElapsedTime: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ElapsedTime()
+}
---
+++ CodeBreaker/Views/UIExtension.swift
@@ -0,0 +1,8 @@
+//
+//  UIExtension.swift
+//  CodeBreaker
+//
+//  Created by Weerachai Anotaipaiboon on 18/2/2569 BE.
+//
+
+import Foundation
